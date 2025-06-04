816e0ef985d0d8704e33e057cc0f066d5c847947
Cristiano Claudson Lautert <cristiano.c.lautert@gmail.com>
2025-06-04 14:56:03 -0300

    Create Guards

--- AgripeWebUI/src/app/app-routing.module.ts
+++ AgripeWebUI/src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { PivotFormComponent } from './components/pivot-form/pivot-form.component
 import { UserFormComponent } from './components/user-form/user-form.component';
 import { SensorFormComponent } from './components/sensor-form/sensor-form.component';
 import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: DashboardComponent },
-  { path: 'pivot', component: PivotFormComponent },
-  { path: 'user', component: UserFormComponent },
-  { path: 'sensor', component: SensorFormComponent }
+  { path: '', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'pivot', component: PivotFormComponent, canActivate: [authGuard] },
+  { path: 'user', component: UserFormComponent, canActivate: [authGuard] },
+  { path: 'sensor', component: SensorFormComponent, canActivate: [authGuard] }
   // {
   //   path: '',
   //   component: DashboardComponent,
--- AgripeWebUI/src/app/components/login/login.component.ts
+++ AgripeWebUI/src/app/components/login/login.component.ts
@@ -17,7 +17,8 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.apiService.login(this.email, this.password)) {
-      this.router.navigate(['/dashboard']);
+      localStorage.setItem('token', 'usuario-logado');
+      this.router.navigate(['/']);
     } else {
       alert('Usuário ou senha inválidos');
     }
